b360e8c14ad8d60dea24180585afe65c513c32ae
MehrabSakal <mhsakaladbc@gmail.com>
2025-11-17 21:27:55 +0600

    added css file

--- src/main/java/com/example/sakal_2207066_gpa_calculator_builder/HelloApplication.java
+++ src/main/java/com/example/sakal_2207066_gpa_calculator_builder/HelloApplication.java
@@ -5,11 +5,16 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.util.Objects;
+
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws Exception {
         FXMLLoader loader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(loader.load(), 620, 440);
+        scene.getStylesheets().add(Objects.requireNonNull(
+                HelloApplication.class.getResource("file.css")).toExternalForm()
+        );
         stage.setTitle("GPA Calculator Builder");
         stage.setScene(scene);
         stage.show();
